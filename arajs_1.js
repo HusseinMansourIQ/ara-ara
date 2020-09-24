ce3284db2795876acd113cce2c17273abfba475c
HusseinDafer <hussein.mansour132.2000.5@gmail.com>
2020-09-24 14:28:46 +0300

    صب

--- arajs_1.js
+++ arajs_1.js
@@ -2,3 +2,12 @@ var marck = 60
 var pass
 pass = marck > 50? true:false
 console.log(pass)
+
+function gg()
+{
+    var com = confirm("this siet is onlay for gays are you gay?")
+    if(com = true)
+    {
+        document.write("heeel nah bro");
+    }
+}
